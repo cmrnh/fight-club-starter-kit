3c4ece6da1e1e5eaf1e94d5af5774636e5d8752d
Cameron Hendrix <cameron@burningbarnlabs.com>
2015-07-22 17:29:59 -0400

    check for `onInstalled` reason before firing popup

--- js/background/app.js
+++ js/background/app.js
@@ -1,20 +1,21 @@
-// 
+//
 // Extension Starter
 // app.js
-// 
+//
 
 // "Unique" identifier for the extension on the user's system
 function getInstanceId() {
-  if (!localStorage.hasOwnProperty("instanceId"))
-    localStorage.instanceId = 'extension-starter' + parseInt(Date.now() * Math.random(), 10);
+  if (!localStorage.hasOwnProperty('instanceId'))
+    localStorage.instanceId = parseInt(Date.now() * Math.random(), 10);
   return localStorage.instanceId;
 }
 
 // Capture the installation event
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function(details) {
 	// Open up a window at installation
-	// WARNING: THIS RUNS EVERY TIME THE EXTENSION IS AUTO-UPDATED
-  chrome.windows.create({ url: "http://localhost/~cameron/example" });
+  if (details.reason === 'install') {
+    chrome.windows.create({ url: 'http://localhost/~cameron/example' });
+  }
 });
 
 // Listen for a message from a pre-permissioned webpage
@@ -22,6 +23,6 @@ chrome.runtime.onMessageExternal.addListener(
 	function(request, sender, sendResponse) {
 		// Do something when a pre-permissioned webpage
 		// attempts to communicate with the extension
-		alert("*Knock Knock from " + getInstanceId() + "*");
+		alert('*Knock Knock from ' + getInstanceId() + '*');
 	}
 );
